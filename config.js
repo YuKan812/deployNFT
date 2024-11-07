13bc32c0dbb1ece8d2c2c01692f22cb22735edde
YuKan812 <65306737+YuKan812@users.noreply.github.com>
2024-11-07 15:23:34 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+export default {
+
+    // connect to ton settings
+    TONCENTER_API_KEY: "88a99a3cddf24890dd3bf5040bf560e6c2343aee9f5707a71786eae2b19c729b",
+    TON_API_URL: "https://toncenter.com/api/v2/jsonRPC",
+    MNEMONIC: ["almost", "cage", "earth", "silly", "point", "helmet", "escape", "mercy", "scrap", "boil", "buddy", "cube", "ten", "exist", "exile", "zone", "toilet", "recipe", "wine", "expect", "token", "summer", "antenna", "misery"],
+
+    // settings
+    SENDER_ADDRESS: "EQCmkbd5fQCNckfk50qU4DxwVchVEKA2XC35FSi3s6GTnGjA", // sender wallet (hub)
+    MIN_AMOUNT: 4, // amount jettons
+    RECEIVER_ADDRESS: "EQD0yywTT0lEWm4wHM_9JJwS4g1QYCW7bg4eUTxFFWKlNbHC", // receiver wallet (keeper)
+
+    TONCENTER_API_KEY_TEST: "2a413f4245a1eb74701894e20f42eedf011171f247f0c59b4d8a05a807985163",
+    TON_API_URL_TEST: "https://testnet.toncenter.com/api/v2/jsonRPC",
+    RECEIVER_ADDRESS_TEST: "0QAr7vDi3Cm-oUGwwSsSaCemXOyBuv_jN8F3DxZtPOV_k7l4",  // keeper
+    SENDER_ADDRESS_TEST: "0QD0yywTT0lEWm4wHM_9JJwS4g1QYCW7bg4eUTxFFWKlNVeN",  // hub
+
+    MNEMONIC_TEST: ["give", "current", "real", "inspire", "pelican", "grit", "mention", "narrow", "taxi", "stem", "dice", "pupil", "mirror", "road", "online", "small", "copy", "despair", "unusual", "alter", "solution", "extra", "result", "lucky"],
+
+    COLLECTION_CONTENT_URI: 'https://github.com/YuKan812/Tokens_img/blob/6a78f93f23332709fdd686e14d7c9a71aae70646/collection.json',
+    COMMON_CONTENT_URL: 'https://github.com/YuKan812/Tokens_img/',
+};
